556a2470cac11b8a8163347cb4562aa7c3ce621d
roberto2002-03-12 <67761057+roberto2002-03-12@users.noreply.github.com>
2022-05-18 19:34:54 -0500

    Agregar archivos de proyecto.

---
+++ Lab09/Lab09/PanContainerClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Lab09
+{
+    public class PanContainerClass : ContentView
+    {
+        double x, y;
+
+        public PanContainerClass()
+        {
+            var panGesture = new PanGestureRecognizer();
+            panGesture.PanUpdated += OnPanUpdated;
+            GestureRecognizers.Add(panGesture);
+        }
+
+        void OnPanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+            switch (e.StatusType)
+            {
+
+                case GestureStatus.Running:
+                    Content.TranslationX = Math.Max(Math.Min(0, x + e.TotalX), -Math.Abs(Content.Width - App.ScreenWidth));
+                    Content.TranslationY = Math.Max(Math.Min(0, y + e.TotalY), -Math.Abs(Content.Height - App.ScreenHeight));
+                    break;
+
+                case GestureStatus.Completed:
+                    x = Content.TranslationX;
+                    y = Content.TranslationY;
+                    break;
+            }
+        }
+    }
+}
